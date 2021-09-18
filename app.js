7cf5439c199b942dfad41c7a469cfb0c4449b753
vishnusj <vishnusj12@gmail.com>
2021-09-18 18:09:34 +0530

    Refactor with module pattern and es6 class

--- app.js
+++ app.js
@@ -1,145 +1,149 @@
-const form = document.getElementById('input-form');
-const formButton = document.getElementById('add-form');
-const formView = document.querySelector('.form-card')
-const bookList = document.querySelector('.books-wrapper');
+const Book = class {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = false;
+    }
 
-let myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
-let newBook;
-
-if (localStorage.getItem('myLibrary') === null) {
-    myLibrary = [
-        new Book('The Hobbit', 'J.R.R. Tolkien', 295),
-        new Book('Harry Potter', 'J.K Rowling', 320),
-        new Book('The Subtle Art of Not Giving a Fuck', 'Mark Manson', 300)
-    ];
-    setItem();
-}
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = false;
-    // this.info = function() {
-    //     return `${this.title} is a book by ${this.author}, ${this.pages} pages`
-    // };
-};
-
-function info(book) {
-    return `${book.title} is a book by ${book.author}, ${book.pages} pages`
-}
-
-// Book.prototype.read = false;
-// Book.prototype.info = function() {
-//     return `${this.title} is a book by ${this.author}, ${this.pages} pages`
-// }
-
-// function setItem() {
-//     localStorage.setItem('myLibrary', JSON.stringify(myLibrary, function replacer(key, value) {
-//         return (key == 'info') ? undefined : value;
-//     }));
-// }
-function setItem() {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
-
-function addToLibrary(e) {
-    {
-        e.preventDefault();
-        const title = (document.getElementById('title')).value;
-        const author = (document.getElementById('author')).value;
-        const pages = (document.getElementById('pages')).value;
-        newBook = new Book(title, author, pages);
-    } {
-        myLibrary.push(newBook);
-        populateBooks(myLibrary, bookList);
-        setItem();
-        formDisplay();
-        form.reset();
-        console.table(myLibrary)
+    info() {
+        return `
+        ${this.title} is a book by ${this.author},
+        ${this.pages}Pages.
+        `;
     }
 };
 
-function populateBooks(myLib, bookView) {
-    const bookCards = document.querySelectorAll('.book-card')
-    bookCards.forEach(bookCard => bookList.removeChild(bookCard));
-    myLib.forEach((book, index) => {
-        book.prototype = Object.create(Book.prototype)
-        book.id = index;
-        const cardContent = `<div class="book-card" data-index=${book.id} >
-                                <div class="card-info-wrapper">
-                                    <h2>${book.title}</h2>
-                                    <h3>${book.author}</h3>
-                                    <h4>${book.pages} Pages</h4>
-                                    <p>${info(book)}</p>
-                                <p id="status-book${book.id}" class ="read-status">${
-                                    book.read ? 'Completed' : 'Not read'}
-                                    </p>
-                                </div>
-                                <div class="card-menu">
-                                <div class="button read" id="read-btn-${book.id}">
-                                    ${
-                                        book.read ?  'Mark as Unread' : 'Mark as Read'
-                                    }
-                                </div>
-                                    <div class="button remove" id="remove-btn-${book.id}">
-                                        Remove
-                                    </div>
-                                </div>
-                        </div>`
+const Library = (function () {
+    let myLibrary = JSON.parse(localStorage.getItem("myLibrary")) || [];
 
-        const element = document.createElement('div');
-        element.innerHTML = cardContent;
-        bookView.appendChild(element.firstChild);
+    function getLibrary() {
+        return myLibrary;
+    }
 
-        const removeButton = document.querySelector(`#remove-btn-${book.id}`);
-        // removeButton.addEventListener('click', (e) => {
-        //     removeBook(book.id)
-        // })
-        removeButton.addEventListener('click', removeBook.bind(book.id), false);
+    function addToLibrary(e) {
+        e.preventDefault();
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        console.log(title, author, pages);
+        let newBook = new Book(title, author, pages);
+        myLibrary.push(newBook);
+        LocalStorage.set();
+    }
+    return {
+        add: addToLibrary,
+        get: getLibrary,
+    };
+})();
 
-        const readButton = document.querySelector(`#read-btn-${book.id}`);
-        const status = document.querySelector(`#status-book${book.id}`)
+const displayController = (function () {
+    const form = document.getElementById("input-form");
+    const formButton = document.getElementById("add-form");
+    const formView = document.querySelector(".form-card");
+    const bookWrapper = document.querySelector(".books-wrapper");
+    const myLibrary = Library.get();
 
-        readButton.addEventListener("click", (e) => {
-            readStatus(book, status, readButton);
-            book.read ? readButton.textContent = 'Mark as Unread' : readButton.textContent = 'Mark as Read'
-            status.textContent = book.read ? 'Completed' : 'Not read';
+    formButton.addEventListener("click", toggleForm);
+    form.addEventListener("submit", addBook);
+    document.addEventListener("DOMContentLoaded", render);
+
+    function addBook(e) {
+        Library.add(e);
+        clear();
+        render();
+    }
+
+    function render() {
+        myLibrary.forEach((book, index) => {
+            book.id = index;
+            const cardContent = `
+                <div class="card-info-wrapper">
+                    <h2>${book.title}</h2>
+                    <h3>${book.author}</h3>
+                    <h4>${book.pages} Pages</h4>
+                    <p id="status-book${book.id}" class ="read-status">${book.read ? "Completed" : "Not read"
+                }</p>
+                </div>
+                <div class="card-menu">
+                    <div class="button read" id="read-btn-${book.id}">
+                        ${book.read ? "Mark as Unread" : "Mark as Read"}
+                    </div>
+                    <div class="button remove" id="remove-btn-${book.id
+                }">Remove</div>
+                </div>
+            `;
+            const element = document.createElement("div");
+            element.classList.add("book-card");
+            element.dataset.index = book.id;
+            element.innerHTML = cardContent;
+            bookWrapper.appendChild(element);
+
+            const removeButton = document.querySelector(`#remove-btn-${book.id}`);
+            removeButton.addEventListener("click", () => { remove(book.id) });
+
+            const readButton = document.querySelector(`#read-btn-${book.id}`);
+            const status = document.querySelector(`#status-book${book.id}`);
+
+            readButton.addEventListener("click", (e) => {
+                toggleRead(book, status, readButton);
+                book.read
+                    ? (readButton.textContent = "Mark as Unread")
+                    : (readButton.textContent = "Mark as Read");
+                status.textContent = book.read ? "Completed" : "Not read";
+            });
         });
+    }
 
-    });
+    function clear() {
+        const bookCards = document.querySelectorAll(".book-card");
+        bookCards.forEach((bookCard) => bookWrapper.removeChild(bookCard));
+    }
 
-};
+    function remove(id) {
+        myLibrary.splice(id, 1);
+        LocalStorage.set();
+        clear();
+        render();
+    }
 
-// bookList.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("remove")) {
-//         const index = e.target.parentElement.parentElement.dataset.index;
-//         removeBook(index);
-//     }
-// });
+    function toggleRead(book) {
+        book.read = !book.read;
+        LocalStorage.set()
+    }
 
-function formDisplay() {
-    form.reset();
-    formView.classList.toggle('toggle-on');
-};
+    function toggleForm() {
+        form.reset();
+        formView.classList.toggle("toggle-on");
+    }
 
-function removeBook(id) {
-    console.log(this);
-    myLibrary.splice(id, 1);
-    setItem();
-    populateBooks(myLibrary, bookList);
-}
+    return {
+        toggleForm,
+        render,
+        clear,
+        remove,
+    };
+})();
 
-function readStatus(book, status, readButton) {
-    book.read = !book.read;
-    setItem();
-    // populateBooks(myLibrary, bookList);
-}
+const LocalStorage = (function () {
+    myLibrary = Library.get();
+    function loadDummy() {
+        if (localStorage.getItem("myLibrary") === null) {
+            myLibrary = [
+                new Book("The Hobbit", "J.R.R. Tolkien", 295),
+                new Book("Harry Potter", "J.K Rowling", 320),
+                new Book("The Subtle Art of Not Giving a Fuck", "Mark Manson", 300),
+            ];
+            setItem();
+        }
+    }
 
-document.addEventListener("DOMContentLoaded", function() {
-    form.addEventListener("submit", function(e) {
-        addToLibrary(e)
-    });
-    formButton.addEventListener('click', formDisplay);
-    populateBooks(myLibrary, bookList);
-});
+    function setItem() {
+        localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    }
+
+    return {
+        get: loadDummy,
+        set: setItem,
+    }
+})();
